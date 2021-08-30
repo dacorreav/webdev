9cc045068cf3e6f4679527f78c1f6546a3aa4b56
David Correa <david.correav@gmail.com>
2021-08-30 08:36:02 -0500

    react keeper app

---
+++ 2021DevBootcamp/keepr/src/public/components/Note.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import { ReactDOM } from "react";
+
+const Note =()=>{
+return (
+<div className="note">
+<h1 > This is the title </h1>
+        <p>This is the paragraph </p>
+    </div>
+)
+}
+export default Note
---
+++ 2021DevBootcamp/keepr/src/public/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Keepr</h1>
+    </header>
+  );
+};
+export default Header;
