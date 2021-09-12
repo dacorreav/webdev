c2a542a33850970d6e51bcfdab041d9ebf81a45e
David Correa <david.correav@gmail.com>
2021-09-12 09:36:53 -0500

    Proyecto terminado

--- Paises-main/src/App.js
+++ Paises-main/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./views/Home";
-import Resultados from "./views/Resultados";
 import Pais from "./views/Pais";
 
 function App() {
@@ -10,8 +9,7 @@ function App() {
       <div className="App">
         {" "}
         <Route exact path="/" component={Home} />{" "}
-        <Route exact path="/resultados" component={Resultados} />{" "}
-        <Route exact path="/pais" component={Pais} />{" "}
+        <Route exact path="/pais/:id" component={Pais} />{" "}
       </div>{" "}
     </BrowserRouter>
   );
--- Paises-main/src/components/Countries.jsx
+++ Paises-main/src/components/Countries.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 
 const countries = (props) => {
   const { flag, name, capital, demonym, region, population, numericCode } =
     props;
-
+  const { path, url } = useRouteMatch();
+  console.log(url);
   return (
-    <div key={numericCode}>
-      <img src={flag} alt={name} style={{ width: "500px", height: "300px" }} />
+    <div key={numericCode} className="card-pais">
+      <img src={flag} alt={name} style={{ width: "200px", height: "150px" }} />
+
       <h1>{name}</h1>
-      <h2>{capital}</h2>
-      <p>{demonym}</p>
-      <p>{region}</p>
-      <p>{population}</p>
+      <h2>Capital: {capital}</h2>
+      <p>Gentilicio: {demonym}</p>
+      <p>Región: {region}</p>
+      <p>Población: {population}</p>
+      <Link to={`/pais/${name}`}>Leer más</Link>
     </div>
   );
 };
