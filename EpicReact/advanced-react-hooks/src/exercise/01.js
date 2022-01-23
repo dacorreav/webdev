ac80dc7af7b0b73bcf509db6bce08cdb6f5db707
David Correa <david.correav@gmail.com>
2022-01-23 16:29:06 -0500

    useReducer

--- EpicReact/advanced-react-hooks/src/exercise/01.js
+++ EpicReact/advanced-react-hooks/src/exercise/01.js
@@ -6,17 +6,27 @@ import * as React from 'react'
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
-  function countReducer(state, newState) {
-    return newState
+  function countReducer(state, {type, step}) {
+    switch (type) {
+      case 'INCREMENT': {
+        return {
+          ...state,
+          count: state.count + step,
+        }
+      }
+      default: {
+        throw new Error(`Unhandled action type: ${type}`)
+      }
+    }
   }
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setState({count: state.count + step})
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{state.count}</button>
 }
 
